f891fb5d1b7a395387c62899c9c62710a33bdfe0
Takana-Norimasa <neuromancer_wg@outlook.jp>
2020-06-09 20:18:10 +0900

    165 C

--- ABC165/C.cpp
+++ ABC165/C.cpp
@@ -5,38 +5,53 @@
 using namespace std;
 typedef long long ll;
 
-struct data{
+struct Req{
 	int Ai;
 	int Bi;
 	int ans;
 	int point;
 };
 
+int N,M,Q;
+int answer=0;
+vector<int> A;
+vector<Req> given;
+
+int dfs(vector<int> A){ 
+	if(A.size() == N+1){ int counter=0;
+		for(auto g:given)
+			if(A[g.Bi] - A[g.Ai] == g.ans) counter+=g.point;
+
+		return counter;
+	}
+
+	A.push_back(A.back());
+	while(A.back()<=M){
+		answer=max(answer,dfs(A));
+		A.back()++;
+	}
+
+	return answer;
+}
+
 int main(void){
 	cin.tie(0);
 	ios::sync_with_stdio(false);
 
-	int N,M,Q;
-	int a,b,c,d;
-	int i,j;
-	int answer=0;
-	int counter=0;
-	data tmp;
-	vector<data> given;
+	int i;
+	struct Req tmp;
 
 	cin >> N >> M >> Q;
 
-	for(i=0;i<N;i++){
+	for(i=0;i<Q;i++){
 		cin >> tmp.Ai >> tmp.Bi >> tmp.ans >> tmp.point;
 		given.push_back(tmp);
 	}
 
-	//sort(given.begin(),given.end());
-	//sort(given.begin(),given.end(),greater<int>());
-	
-	answer=0;
+	A.resize(1,1);
+	dfs(A);
+
 	cout << answer << endl;
 
 	return 0;
 }
-
