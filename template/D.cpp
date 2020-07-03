b53a5fa0153a6aa3c32053490ece380eef5c498f
Takana-Norimasa <neuromancer_wg@outlook.jp>
2020-07-03 23:27:07 +0900

    fix template

--- template/D.cpp
+++ template/D.cpp
@@ -1,33 +1,28 @@
 #include <bits/stdc++.h>
-#define SIZE 10
 #define PI 3.141592653589793
-
+#define rep(i,n) for (int i=0;i<(n);i++)
 using namespace std;
-typedef long long ll;
+using ll = long long;
 
 int main(void){
 	cin.tie(0);
 	ios::sync_with_stdio(false);
 
 	int N;
-	int i,j;
-	int tmp;
-	int answer=0;
-	int counter=0;
-	vector<int> given;
+	int i,k;
+	int ans=0;
+	int ctr=0;
 
 	cin >> N;
 
-	for(i=0;i<N;i++){
-		cin >> tmp;
-		given.push_back(tmp);
-	}
+	vector<int> gv(N);
+	rep(i,N) cin >> gv[i];
 
-	//sort(given.begin(),given.end());
-	//sort(given.begin(),given.end(),greater<int>());
+	//sort(gv.begin(),gv.end());
+	//sort(gv.begin(),gv.end(),greater<int>());
 	
-	answer=0;
-	cout << answer << endl;
+	ans=0;
+	cout << ans << endl;
 
 	return 0;
 }
