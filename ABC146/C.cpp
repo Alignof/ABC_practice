7fb6de48ec47c3fffac332cae8d3c194f9342c4e
Takana-Norimasa <neuromancer_wg@outlook.jp>
2020-08-14 23:39:01 +0900

    146 C

--- ABC146/C.cpp
+++ ABC146/C.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 #define PI 3.141592653589793
 #define rep(i,n) for (int i=0;i<(n);i++)
-#define MAX 1000000000
+#define MAX 1e9
 using namespace std;
 using ll = long long;
 
@@ -17,32 +17,24 @@ int main(void){
 
 	int i,k;
 	ll A,B,X;
+	ll left,right,mid;
 	ll N=0;
 	ll price=0;
 
 	cin >> A >> B >> X;
-
-	if(A+B>X){
-		cout << 0 << endl;
-		return 0;
-	}
-
-	N=((X/A)<(MAX)?(X/A):MAX);
-	while(1){
-		price=A*N + B*digit(N);
-		//cout << N << " " << digit(N) << " " << price << endl;
-		if(price<=X) break;
-		if((A*(N/10) + B*digit(N/10)) > X) N/=10;
-
-		N--;
-	}
 	
-	if(N>MAX)
-		cout << MAX << endl;
-	else if(N<0)
-		cout << 0 << endl;
-	else
-		cout << N << endl;
+	left=0;
+	right=MAX+1;
+
+	while(right-left>1){
+		mid=(left+right)/2;
+		price=A*mid + B*digit(mid);
+
+		if(price<=X) left=mid;
+		else right=mid;
+	}
+
+	cout << left << endl;
 
 	return 0;
 }
