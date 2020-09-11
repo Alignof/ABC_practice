9288cfee85c4e75f8bd2ac0388f8b39a699d66aa
Takana-Norimasa <neuromancer_wg@outlook.jp>
2020-09-11 23:35:31 +0900

    177 D

--- ABC177/D.cpp
+++ ABC177/D.cpp
@@ -4,11 +4,31 @@
 using namespace std;
 using ll = long long;
 
+struct UnionFind{
+	vector<int> d;
+	UnionFind(int n=0): d(n,-1) {}
+	int find(int x){
+		if(d[x]<0) return x;
+		return d[x]=find(d[x]);
+	}
+	bool unite(int x,int y){
+		x=find(x);
+		y=find(y);
+		if(x==y) return false;
+		if(d[x]>d[y]) swap(x,y);
+
+		d[x]+=d[y];
+		d[y]=x;
+		return true;
+	}
+	bool same(int x,int y){return find(x)==find(y);}
+	int size(int x){return -d[find(x)];}
+};
+
 int main(void){
 	cin.tie(0);
 	ios::sync_with_stdio(false);
 
-	int x,y;
 	int N,M;
 	int i,k;
 	int ans=0;
@@ -16,20 +36,15 @@ int main(void){
 
 	cin >> N >> M;
 
-	vector<set<int>> fri(N+1);
-
+	UnionFind uni(N);
 	rep(i,M){
+		int x,y;
 		cin >> x >> y;
-		fri[x].insert(y);
-		fri[y].insert(x);
+		x--;y--;
+		uni.unite(x,y);
 	}
 
-	for(auto& s:fri){
-		//cout << s.size() << endl;
-		if(ans<s.size()) ans=s.size();
-	}
-	
-	ans++;
+	rep(i,N) ans=max(ans,uni.size(i));
 	cout << ans << endl;
 
 	return 0;
